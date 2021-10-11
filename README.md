29f24cb5d289f13aaf0e118740c507667eab4f3a
Ismeny Saguilan <menysag2@gmail.com>
2021-10-11 07:37:50 -0500

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Homework-6-Server-Side-APIs-Weather-Dashboard
+
+## Description
+This project is a work day planner for regular business hours. You are able to type what you have planned for each hour. There is special color coding to effectively display the past, present, and future hours and what you have planned. This project uses local storage to keep the input placed in the time blocks. 
+
+
+### Table of Contents 
+* Installation
+* Usage
+* Credits
+* License
+* Contributions
+* Test
+
+## Installation
+Go to URL : ***https://ismeny.github.io/Homework-5-Third-Party-APIs/***
+
+![ScreenShot](screenshot.png)
+
+
+## Usage
+When opening the work day planner you are presented with the date at the top of the page. You can then plan any work events/ or make notes for yourself. The planner is suited for regular business hours from 9am -5pm. There is a special color coding that easily displays the past, present, and future hours. When you type into the work day planner you will be able to refresh the page and still see your tasks. 
+
+## Credits
+Received help from:
+* Jose Sachango- Tutor
+* Sue Lee - peer    
+* Jayla De'nae - peer
+* Ron - peer
+* Stephanie Cortez - peer
+* Ethan Cho - peer
+
+## License
+MIT
+
+## Contributions
+n/a
+## Test
+n/a
+## Questions
+Github: Ismeny Saguilan - https://github.com/Ismeny 
+
+
+
+Email: menysag2@gmail.com
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// global variables
 var myApi = "e74325500ef2f7e58fc93f84b3d13924";
-var cityName = "austin";
-
-var part = "minutely,hourly"
+var cityName = "";
+var response;
+var part = "minutely,hourly";
 
 var latitude;
 var longitude;
@@ -9,8 +10,67 @@ var longitude;
 var oneCallApi;
 var fiveDayApi;
 
+// get main elements by id
+var mainDiv = document.getElementById("main")
+var userInput = document.getElementById("city-name");
+var submitBtn = document.getElementById("submit");
 
-function getCoordinates() {
+//get ul into variable savedSearches
+var savedSearches = document.getElementById("saved-searches");
+
+
+var storageList = [];
+
+var userLocalStorage = JSON.parse(localStorage.getItem('userLocalStorage'));
+console.log(userLocalStorage);
+
+if (userLocalStorage !== null) {
+  for (var item of userLocalStorage) {
+      storageList.push(item);
+      var newListItem = document.createElement('li');
+      newListItem.setAttribute("class", "list-group-item");
+      newListItem.textContent = item;
+      newListItem.addEventListener("click", function(event) {
+        mainDiv.innerHTML = "";
+        cityName = event.target.textContent;
+        getCoordinates(cityName);
+        
+      });
+      console.log(newListItem);
+      
+      savedSearches.appendChild(newListItem);
+      console.log(savedSearches);
+  }
+}
+
+// submit Button
+submitBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  cityName = "";
+  mainDiv.innerHTML = "";
+  response = userInput.value;
+  cityName = response;
+  getCoordinates(cityName);
+
+  var newListItem = document.createElement('li');
+  newListItem.setAttribute("class", "list-group-item");
+  newListItem.textContent = cityName;
+  newListItem.addEventListener("click", function(event) {
+    mainDiv.innerHTML = "";
+    cityName = event.target.textContent;
+    getCoordinates(cityName)
+  })
+
+  savedSearches.appendChild(newListItem);
+  //if there is a result push newListItem value onto the storage list array
+  storageList.push(newListItem.textContent);
+  localStorage.setItem('userLocalStorage', JSON.stringify(storageList));
+});
+
+
+
+
+function getCoordinates(cityName) {
   // assign API url to five day api variable
   fiveDayApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myApi}`
 
@@ -33,7 +93,7 @@ function getCoordinates() {
     })
 }
 
-getCoordinates();
+
 
 function getOneCallApi (latitude, longitude) {
   oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=${part}&appid=${myApi}`
@@ -49,7 +109,7 @@ function getOneCallApi (latitude, longitude) {
             var currentHumidity = data.current.humidity
             // get temperature
             console.log(data.current.temp);
-            var currentTemperature = (data.current.temp-273.15)*(9/5) + 32;
+            var currentTemperature = Math.round((data.current.temp-273.15)*(9/5) + 32);
             console.log(currentTemperature);
             // get uv index
             console.log(data.current.uvi);
@@ -67,15 +127,42 @@ function getOneCallApi (latitude, longitude) {
             // get the current weather status
             console.log(data.current.weather[0].description);
             var currentWeatherStatus = data.current.weather[0].description
-          // get the icon
+            // get the icon
             var currentIcon = data.current.weather[0].icon;
-            console.log(currentIcon)
-            
-            // source for icon url
-            var currentIconUrl = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`
-            console.log(currentIconUrl);
+            console.log(currentIcon);
 
-            console.log(data.daily)
+
+            // source for icon url
+            var currentIconUrl = `https://openweathermap.org/img/wn/${currentIcon}@2x.png`
+
+            // creating card dynamically
+            var currentWeatherResults = document.createElement('div');
+            // give the div for the current weather classes
+            currentWeatherResults.setAttribute("class", "container d-flex justify-content-center");
+            
+            currentWeatherResults.innerHTML = `
+            <div class="card border-success mb-3" style=" min-width: 30rem">
+              <h5 class="card-title">${cityName}</h5> 
+              <div> <img src="${currentIconUrl}" alt ="weather icon"></div>
+              <div class="card-body text-success">
+                <p class="card-text">${currentDateTime}</p>
+                <p class="card-text">${currentWeatherStatus}</p>
+                <p class="card-text">${currentTemperature} \xB0F</p>
+                <p class="card-text">UVI: ${currentUvi}</p>
+                <p class="card-text">${currentHumidity} %</p>
+                <p class="card-text">${currentWindSpeed} mph</p>
+             </div>
+            </div>`
+
+            mainDiv.appendChild(currentWeatherResults);
+
+            
+            var dailyWeatherResults = document.createElement('div');
+             dailyWeatherResults.setAttribute("class", "container d-flex justify-content-center flex wrap")
+             mainDiv.appendChild(dailyWeatherResults);
+            
+
+          
 
             for(var i = 1; i <= 5; i++) {
               console.log(data.daily[1]);
@@ -84,10 +171,11 @@ function getOneCallApi (latitude, longitude) {
               var dailyHumidity = data.daily[i].humidity;
               // get day time temperature
               console.log(data.daily[i].temp.day);
-              var dailyTemperature = data.daily[i].temp.day;
+              var dailyTemperature = Math.round((data.daily[i].temp.day-273.15)*(9/5) + 32);
               // get date time
               console.log(data.daily[i].dt);
-              var dailyDateTime = new Date(data.daily[i].dt);
+              var dailyDateTime = new Date(data.daily[i].dt*1000);
+              dailyDateTime = dailyDateTime.toLocaleDateString("en-US")
               console.log(dailyDateTime);
               // daily weather status
               console.log(data.daily[i].weather[0].description);
@@ -103,6 +191,36 @@ function getOneCallApi (latitude, longitude) {
               // get uvi
               var dailyUvi = data.daily[i].uvi
               console.log(data.daily[i].uvi);
+
+
+
+              var dailyIconUrl = `http://openweathermap.org/img/wn/${dailyIcon}@2x.png`
+            console.log(dailyIcon);
+
+
+               // creating card dynamically
+             
+
+             var dailyWeatherDiv = document.createElement('div');
+             
+
+             dailyWeatherDiv.innerHTML = `
+             <div class="card border-success mb-3" style=" max-width: 12rem; min-width: 12rem;"> 
+             <div class="card-header bg transparent border-success">${dailyDateTime}</div>
+               <div> <img src="${dailyIconUrl}" alt ="weather icon"></div>
+               <div class="card-body text-success">
+                 <p class="card-text">${dailyDateTime}</p>
+                 <p class="card-text">${dailyWeatherStatus}</p>
+                 <p class="card-text">${dailyTemperature} \xB0F</p>
+                 <p class="card-text">UVI: ${dailyUvi}</p>
+                 <p class="card-text">${dailyHumidity} %</p>
+                 <p class="card-text">${dailyWindSpeed} mph</p>
+              </div>
+             </div>`
+ 
+             
+             dailyWeatherResults.appendChild(dailyWeatherDiv);
+            
             }
           })
         }
